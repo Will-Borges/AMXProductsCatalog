9ac3c7926ced6da2e72809273327a964a794889b
William Borges <36491847+Will-Borges@users.noreply.github.com>
2024-04-02 18:53:27 -0300

    13- add Settings

---
+++ AMXProductsCatalog/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace AMXProductsCatalog
+{
+    public static class Settings
+    {
+        public static string Secret = "6cd3556deb0da54bca060b4c39479839e4099838f212f115f27c0a4c21b8d7f6";
+    }
+}
