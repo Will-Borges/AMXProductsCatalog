77a2ecaa4e0cc413f9457243b1ed2f5db879d7c1
William Borges <36491847+Will-Borges@users.noreply.github.com>
2024-04-03 15:13:25 -0300

    20 - retirando swaggerSubTypes

--- AMXProductsCatalog.Core.Domain/Abstractions/Products/BaseProduct.cs
+++ AMXProductsCatalog.Core.Domain/Abstractions/Products/BaseProduct.cs
@@ -1,15 +1,8 @@
-﻿using Swashbuckle.AspNetCore.Annotations;
-using System.Runtime.Serialization;
-
-namespace AMXProductsCatalog.Core.Domain.Abstractions.Products
+﻿namespace AMXProductsCatalog.Core.Domain.Abstractions.Products
 {
-    using AMXProductsCatalog.Core.Domain.Entities.Products;
-
-
-    [SwaggerSubTypes(typeof(CarProductEntity), Discriminator = "productType")]
     public class BaseProduct
     {
-        public long Id { get; set; } //priv
+        public long Id { get; set; }
         public decimal Price { get; set; }
     }
 }
